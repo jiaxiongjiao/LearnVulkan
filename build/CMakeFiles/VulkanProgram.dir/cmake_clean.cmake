09ec40a17f3dee1e239f61c6ee7b9362971007a6
JiaxiongJiao <jiaxiongjiao@icloud.com>
2021-12-16 19:39:29 -0500

    First Commit

---
+++ build/CMakeFiles/VulkanProgram.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanProgram.dir/src/main.cpp.o"
+  "CMakeFiles/VulkanProgram.dir/src/main.cpp.o.d"
+  "VulkanProgram"
+  "VulkanProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/glfw3/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/cocoa_init.m" "glfw3/src/CMakeFiles/glfw.dir/cocoa_init.m.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/cocoa_init.m.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/cocoa_joystick.m" "glfw3/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/cocoa_monitor.m" "glfw3/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/cocoa_time.c" "glfw3/src/CMakeFiles/glfw.dir/cocoa_time.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/cocoa_time.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/cocoa_window.m" "glfw3/src/CMakeFiles/glfw.dir/cocoa_window.m.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/cocoa_window.m.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/context.c" "glfw3/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/egl_context.c" "glfw3/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/init.c" "glfw3/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/input.c" "glfw3/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/monitor.c" "glfw3/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/nsgl_context.m" "glfw3/src/CMakeFiles/glfw.dir/nsgl_context.m.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/nsgl_context.m.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/osmesa_context.c" "glfw3/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/posix_thread.c" "glfw3/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/vulkan.c" "glfw3/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/Users/jiaxiongjiao/LearnVulkan/glfw3/src/window.c" "glfw3/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "glfw3/src/CMakeFiles/glfw.dir/window.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
